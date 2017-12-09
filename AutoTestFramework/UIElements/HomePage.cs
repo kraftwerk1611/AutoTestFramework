08de49440ac342f7316e231cd6b0315d5612363d
kraftwerk1611 <kraftwerk1611@gmail.com>
2017-12-09 12:45:24 -0500

    ver-0.1: first commits. Just started

---
+++ AutoTestFramework/UIElements/HomePage.cs
@@ -0,0 +1,12 @@
+﻿using OpenQA.Selenium.support.PageObject;
+
+namespace AutoTestFramework.UIElements
+{
+    public class HomePage
+    {
+        public HomePage()
+        {
+         PageFactory
+        } 
+    }
+}
